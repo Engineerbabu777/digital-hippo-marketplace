61904b53681906b07f4db97a2410c6a968d8aee8
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-03-13 17:28:24 +0500

    added-hero-section

---
+++ src/components/MaxWidthWrapper.tsx
@@ -0,0 +1,18 @@
+
+
+
+import { cn } from '@/lib/utils'
+import React from 'react'
+
+type Props = {
+    className?:string,
+    children:React.ReactNode,
+}
+
+export default function MaxWidthWrapper({children,className}: Props) {
+  return (
+    <div className={cn('mx-auto w-full max-w-screen-xl px-2.5 md:px-20',className)}>
+{children}
+    </div>
+  )
+}
