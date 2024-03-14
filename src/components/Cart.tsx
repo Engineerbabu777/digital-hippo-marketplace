f947c6e16acb240fdb8482b7edd2f82db08e56bd
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-03-14 21:37:35 +0500

    part-2-conditional-sign-login

---
+++ src/components/Cart.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+
+import React from 'react'
+
+type Props = {}
+
+export default function Cart({}: Props) {
+  return (
+   <>
+   
+   </>
+  )
+}
--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,19 +1,38 @@
 'use client'
 
 import { PRODUCT_CATEGORIES } from '@/config'
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import NavItem from './NavItem'
+import { useOnClickOutside } from '@/hooks/use-on-click-outside'
 
 type Props = {}
 
 export default function NavItems ({}: Props) {
-    const [activeIndex, setActiveIndex] = useState<null|number>(null);
+  const [activeIndex, setActiveIndex] = useState<null|number>(null);
 
-    const isAnyOpen = activeIndex != null;
+  const isAnyOpen = activeIndex != null;
+
+  const navRef = useRef<HTMLDivElement | null>(null);
+
+  useOnClickOutside(navRef, () => setActiveIndex(null))
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      if (e.key == 'Escape') {
+        setActiveIndex(null)
+      }
+    }
+
+    document.addEventListener('keydown', handler)
+
+    return () => {
+      document.removeEventListener('keydown', handler)
+    }
+  }, [])
 
   return (
     <>
-      <div className='flex gap-4 h-full'>
+      <div className='flex gap-4 h-full' ref={navRef}>
         {PRODUCT_CATEGORIES.map((category, i) => {
           const handleOpen = () => {
             if (activeIndex === i) {
@@ -26,7 +45,13 @@ export default function NavItems ({}: Props) {
           const isOpen = i === activeIndex
           return (
             <>
-              <NavItem category={category} handleOpen={handleOpen} isOpen={isOpen} key={category.value} isAnyOpen={isAnyOpen}/>
+              <NavItem
+                category={category}
+                handleOpen={handleOpen}
+                isOpen={isOpen}
+                key={category.value}
+                isAnyOpen={isAnyOpen}
+              />
             </>
           )
         })}
